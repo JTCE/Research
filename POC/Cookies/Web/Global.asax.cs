f83678f516af296bf85060166f88891c41450cb6
Roel van Lisdonk <roelvanlisdonk@live.com>
2016-05-20 11:58:13 +0200

    Initial commit

---
+++ POC/Cookies/Web/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public const string TwoFactorCookieName = "TwoFactor";
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HandleTwoFactorCookie(HttpContext.Current.Request.Cookies, HttpContext.Current.Response.Cookies);
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void HandleTwoFactorCookie(HttpCookieCollection requestCookies, HttpCookieCollection responseCookies)
+        {
+            
+            var cookieExists = requestCookies.AllKeys.Contains(TwoFactorCookieName);
+            if (cookieExists)
+            {
+                var cookie = requestCookies[TwoFactorCookieName];
+                HttpContext.Current.Response.Write("TwoFactor cookie:" + cookie.Value);
+            }
+            else
+            {
+                var cookie = new HttpCookie(TwoFactorCookieName);
+                cookie.Value = "104,105";
+                cookie.Expires = DateTime.Now.AddMinutes(10);
+                cookie.HttpOnly = true;
+                responseCookies.Add(cookie);
+            }
+        }
+    }
+}
