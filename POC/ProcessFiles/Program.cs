1fee48dec6ab87563bfa9a8c54923d8f33c7bda5
Roel van Lisdonk <roelvanlisdonk@live.com>
2016-07-02 09:14:39 +0200

    worked on style extraction

--- POC/ProcessFiles/Program.cs
+++ POC/ProcessFiles/Program.cs
@@ -9,20 +9,26 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        /*
+         * Tasks
+         * - Create SASS files foreach CSHTML file, that contains a html tag with a style attribute.
+         * - Create XML that can be used to add each created SASS file to a *.csproj file with "Build Action" set to "None".
+         */
         public static void Main(string[] args)
         {
             Console.WriteLine("Processing files started.");
             var fs = new FileSystem();
-            var projectFolder = new DirectoryInfo(@"C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web");
             var info = new ProcessCshtmlFilesInfo();
-
-            var files = fs.FindFiles(projectFolder, "*.cshtml");         
+            info.ProjectFolder = new DirectoryInfo(@"C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web");
+            
+            var files = fs.FindFiles(info.ProjectFolder, "*.cshtml");         
             foreach (FileInfo file in files)
             {
                 Task task = fs.ProcessCshtmlFile(file, info);
                 task.Wait();
             }
-
+            Console.WriteLine($"<ItemGroup>{info.CsprojXml}</ItemGroup>");
+            Console.WriteLine(info.BundleCsIncludes.ToString());
             Console.WriteLine($"Total *.cshtml files = {info.FileCounter}");
             Console.WriteLine($"Total *.cshtml files containing the text 'style=' = {info.StyleFileCounter}");
             Console.WriteLine($"Total 'style =' text count {info.StyleTextCounter}");
@@ -41,10 +47,9 @@ namespace ConsoleApplication
             }
         }
 
-        /* Find files uses EnumerateFiles, because this is faster then GetFiles. 
-         */
         public IEnumerable<FileInfo> FindFiles(DirectoryInfo folder, string pattern)
         {
+            // Note: using EnumerateFiles is faster then using "GetFiles".
             return folder.EnumerateFiles(pattern, SearchOption.AllDirectories);
         }
 
@@ -64,18 +69,97 @@ namespace ConsoleApplication
                         info.StyleFileCounter++;
 
                         // Create sass file.
-                        var scssPath = Path.ChangeExtension(file.FullName, ".scss");
-                        await CreateOrUpdateFile(scssPath, string.Empty);                  
-                        Console.WriteLine(file.FullName);
-                        Console.WriteLine(scssPath);
+                        var scssPath = Path.ChangeExtension(file.FullName, ".scss");                        
+                        await CreateOrUpdateFile(scssPath, "@import \"./App/Styles/variables\";" + Environment.NewLine + Environment.NewLine);                  
+
+                        AddCsprojXml(scssPath, info);
+                        AddBundleCsPaths(scssPath, info);
                     }              
                 }
         }
+
+        /* Create text that can be used to add the css file to the BundleConfig.cs.
+         */
+        public void AddBundleCsPaths(string scssPath, ProcessCshtmlFilesInfo info) {
+            var cssPath = Path.ChangeExtension(scssPath, ".css");
+            if(File.Exists(cssPath)) {
+                Console.WriteLine(cssPath + " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+
+            // Replace leading "_" in filename, because sass can't handle that.
+            var folder = Path.GetDirectoryName(cssPath);
+            var name = Path.GetFileName(cssPath);
+            if(name.StartsWith("_")) {
+                name = name.Substring(1);
+            }
+            cssPath = Path.Combine(folder, name);
+
+            // If file exists it should not be added to the bundle.
+            // But the contents of the file should be placed in the *.scss file
+            if(File.Exists(cssPath)) {
+                Console.WriteLine(cssPath + " <============================================");
+            }
+            else {
+                // Make path relative to project root.
+                string path = cssPath.Replace(info.ProjectFolder.FullName + "\\", string.Empty);
+                
+                // Convert from Windows file paths to URL.
+                path = path.Replace(@"\", "/");
+
+                // Create include string.
+                string include = $"\"~/{path}\",";           
+                info.BundleCsIncludes.AppendLine(include);
+            }       
+        }
+
+        /* Create xml text that can be use to add sass file to *.csproj with "Build Action" set to "None".
+         */
+        public void AddCsprojXml(string scssPath, ProcessCshtmlFilesInfo info) {
+            string path = scssPath.Replace(info.ProjectFolder.FullName + "\\", string.Empty);
+            string xml = $"<None Include=\"{path}\" />";           
+            info.CsprojXml.AppendLine(xml);
+        }
     }
 
-    public struct ProcessCshtmlFilesInfo {
+    public class ProcessCshtmlFilesInfo {
+        public ProcessCshtmlFilesInfo() {
+            this.BundleCsIncludes = new StringBuilder(string.Empty);
+            this.CsprojXml = new StringBuilder(string.Empty);
+        }
+        public StringBuilder BundleCsIncludes { get; set; }
+        public StringBuilder CsprojXml { get; set; }
         public int FileCounter { get; set; }
+        public DirectoryInfo ProjectFolder { get; set; }
         public int StyleTextCounter { get; set; }
         public int StyleFileCounter { get; set; }
     }
+
+
+
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Administratie\Verlof\Index.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Administratie\ZiekteMelding\Index.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Contact\Contact.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Dossiers\Verzuimkalender.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\GebruikersBeheer\GebruikersBeheer.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\GebruikersBeheer\Index.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\MijnProfiel\MijnContactgegevens.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\MijnTools\Index.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Rapporten\NoRiskpolis.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Rapporten\UitDienst.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Rapporten\Vragenlijst.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Rapporten\WAOStatus.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Rapporten\WgaStatus.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Rapporten\ZiekUitDienst.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Registratie\Aanmelden.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Settings\ExtraRechten.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Settings\Modules.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Settings\TaakDefinitie.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Settings\VerzuimProtocol.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Shared\PdfNotities.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\Shared\PdfVerzuimverlof.css <============================================
+// C:\Projects\ZvdZ\zvdzonline\Source\ZvdZOnline\ZvdZOnline.Web\App\SpreekuurAfspraak\Details.css <============================================
+
+
+
+
 }
